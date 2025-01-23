63f93677db497e2ab66f4388d1fd3e4ceb383207
PanzerWeb <selwynvillar@gmail.com>
2025-01-23 08:45:59 +0800

    Added Background Image
    Added: Guest.css for guest layout
    Added: Background Image

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,9 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/guest.css'])
     </head>
-    <body class="font-sans text-gray-900 antialiased bg-gradient-to-r from-purple-900 via-purple-700 to-purple-500">
+    <body class="font-sans text-gray-900 antialiased">
         <div class="flex flex-col md:flex-row gap-6 items-center justify-center min-h-screen">
             {{ $slot }}
         </div>
