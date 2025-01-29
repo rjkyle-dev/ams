252a60153d5de6e6899ee13c3eb2ce3fa63eedff
RJ Kyle G Labrador <kyledev10282001@gmail.com>
2025-01-29 14:12:50 +0800

    Add hello world

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import './bootstrap';
-
+import { helloWorld } from './console.js';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+window.helloWorld = helloWorld;
 
