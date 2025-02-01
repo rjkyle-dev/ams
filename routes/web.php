9085312fdc849dda99892136c80403322a9bb752
John Lyold Lozada <lozadajohnlyoldcastro12705@gmail.com>
2025-02-01 19:24:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\LogController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\StudentAttendanceController;
-use App\Http\Controllers\StudentController;
-use App\Models\StudentAttendance;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AdminCodeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/admin-code', function () {
     return view('auth.admin-code');
@@ -28,30 +26,9 @@ Route::get('/register', function () {
 })->name('register');
 
 Route::middleware('auth')->group(function () {
-
-    // DASHBOARD RELATED ROUTES
-    Route::get('/dashboard', [DashboardController::class, 'viewDashboard'])->name('dashboard');
-
-
-    // PROFILE RELATED ROUTES
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // LOGS RELATED ROUTES
-    Route::get('/logs', [LogController::class, 'viewLogs'])->name('logs');
-
-
-    // STUDENT RELATED ROUTES
-    Route::post('/addStudent', [StudentController::class, 'create'])->name('addStudent');
-    Route::get('/students', [StudentController::class, 'view'])->name('students');
-
-    // ATTENDANCE RELATED ROUTES
-    Route::get('/attendance', [StudentAttendanceController::class, 'view'])->name('attendance');
-    // Route::post
-
-    // EVENTS RELATED ROUTES
-    Route::post('/addEvent', [EventController::class, 'create'])->name('addEvent');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
