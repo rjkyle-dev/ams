4619630c1c96a35e7ba870e9e41e59ab97308a2f
John Lyold Lozada <lozadajohnlyoldcastro12705@gmail.com>
2025-02-02 16:33:31 +0800

    Composer and Vite update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/css/guest.css',
-                'resources/js/axios.js'
+                'resources/js/axios.js',
+                'resources/js/dashboard.js'
             ],
             refresh: true,
         }),
