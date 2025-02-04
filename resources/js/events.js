a81faad5636ba0a78e1ec7c780d9ce7505433d76
Nicko Enargan <donenargan@gmail.com>
2025-02-04 11:15:44 +0800

    added things

---
+++ resources/js/events.js
@@ -0,0 +1,25 @@
+window.editEvent = editEvent;
+window.deleteEvent = deleteEvent;
+
+
+
+
+
+
+
+
+
+
+
+
+function editEvent(data){
+    console.log('editing event')
+    console.log(data)
+}
+
+
+function deleteEvent(data){
+    console.log('deleting event')
+    console.log(data)
+
+}
