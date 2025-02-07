c709d89e2edb68c60c0cbd414280c074b5fface4
John Lyold Lozada <lozadajohnlyoldcastro12705@gmail.com>
2025-02-07 21:00:46 +0800

    Update logs.blade.php

--- resources/views/pages/logs.blade.php
+++ resources/views/pages/logs.blade.php
@@ -7,22 +7,19 @@
     </x-slot>
 
     {{-- Action buttons --}}
-    <div class="mb-4 py-4 px-2 rounded-md flex justify-end items-center w-full">
+    <div class="bg-white mb-4 py-4 px-2 rounded-md">
         <div class="flex gap-3">
-            <button class="bg-violet-600 hover:bg-violet-700 text-white rounded-md px-2 text-[15px] flex p-3 items-center">
            <a href="{{ route('logs.pdf') }}" class="bg-violet-600 hover:bg-violet-700 text-white rounded-md px-5 text-2xl flex p-3 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-6">
+                    stroke="currentColor" class="size-9">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
                 </svg>
                 Generate Report
-          </a>
-            </button>
-            <button class="bg-violet-600 hover:bg-violet-700 text-white rounded-md px-5 text-[15px] flex p-3 items-center">
-                
+            </a>
+            <button class="bg-violet-600 hover:bg-violet-700 text-white rounded-md px-5 text-2xl flex p-3 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-6">
+                    stroke="currentColor" class="size-9">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
@@ -33,7 +30,7 @@
     </div>
 
     {{-- Content --}}
-    <div class="bg-white p-3 rounded-md">
+    <div class="bg-white p-3">
         <div class="flex justify-between mb-3">
             <div class="w-full">
                 {{-- SELECTION FORM --}}
